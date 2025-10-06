2e9e282f8d6ce486af014d14cc06040270cf50f1
Hamza Bouzian <bouzianh78@gmail.com>
2025-10-06 19:04:19 +0100

    app initialization

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,1 +1,16 @@
+import express from "express";
+import cors from "cors";
+import busRoutes from "./routes/busRoute.ts";
+import stopRoutes from "./routes/stopRoute.ts";
+import trackRoutes from "./routes/trackRoute.ts";
 
+const app = express();
+
+app.use(cors());
+app.use(experss.json());
+
+app.use("/api/buses", busRoutes);
+app.use("/api/stops", stopRoutes);
+app.use("/api/tracks", trackRoutes);
+
+export default app;
