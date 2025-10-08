34c625a4880c1d7295c16f7bf3368efeceb53e99
Hamza Bouzian <bouzianh78@gmail.com>
2025-10-08 17:55:18 +0100

    add routes for (bus, stop, track)

--- backend/src/routes/busRoute.ts
+++ backend/src/routes/busRoute.ts
@@ -1,9 +1,17 @@
 import { Router } from "express";
-import { getAllBuses, createBus } from "../controllers/busController.ts";
+import {
+    createBus,
+    getBusById,
+    updateBus,
+    deleteBus, 
+} from "../controllers/busController";
 
 const router = Router();
 
-router.get('/', getAllBuses);
-router.post('/', createBus);
+// CRUD routes (create, read, update, delete)
+router.post("/", createBus);
+router.get("/:id", getBusById);
+router.put("/:id", updateBus);
+router.delete("/:id", deleteBus);
 
 export default router;
--- backend/src/routes/trackRoute.ts
+++ backend/src/routes/trackRoute.ts
@@ -1,5 +1,16 @@
 import { Router } from "express";
+import {
+    createTrack,
+    getAllTracksByBus,
+    getLatestTrack,
+    getTrackNearBy
+} from "../controllers/trackController";
 
 const router = Router();
+
+router.post('/', createTrack);
+router.get('/:id', getAllTracksByBus);
+router.get('/:id', getLatestTrack);
+router.get('/:id', getTrackNearBy);
 
 export default router;
